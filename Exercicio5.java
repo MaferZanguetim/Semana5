ecac354900c1cc5b0566a0992dd124d2cf258861
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-03-28 19:27:52 +0000

    Exercicio5

---
+++ Exercicio5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Exercicio5{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int codigo[] = new int[5];
+        String nome[]= new String[5];
+        double preco[]=new double[5];
+        for (int i=0; i<preco.length; i++){
+            System.out.println("Digite o codigo do produto");
+            codigo[i]=sc.nextInt();
+            System.out.println("Digite o nome do produto");
+            nome[i]=sc.next();
+            System.out.println("Digite o preço do produto");
+            preco[i]=sc.nextDouble();
+        }
+        for (int i=0; i<preco.length; i++){
+            System.out.println("Codigo: "+codigo[i]);
+            System.out.println("Nome: "+nome[i]);
+            if(preco[i]>120){
+                System.out.println("Preço: "+(preco[i]+20));
+            }
+            else{
+                System.out.println("Preço: "+preco[i]);
+            }
+        }
+
+    }
+}
